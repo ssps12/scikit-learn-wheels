cf4d12a1614bb480d2001975f0a82ae04c52bd07
ssps12 <38716735+ssps12@users.noreply.github.com>
2020-08-04 16:46:46 +0530

    Create install_conda_aarch64.sh

---
+++ azure/install_conda_aarch64.sh
@@ -0,0 +1,11 @@
+set -v -e
+
+# Install Miniconda
+wget -q "https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh" -O archiconda.sh
+chmod +x archiconda.sh
+./archiconda.sh -b -p /opt/conda
+export PATH="/opt/conda/bin:$PATH"
+conda --version
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
