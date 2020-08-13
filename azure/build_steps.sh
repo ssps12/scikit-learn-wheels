8d8472b3a55020ccb861b072bd52bf5395e6abf8
ssps12 <38716735+ssps12@users.noreply.github.com>
2020-08-13 12:56:44 +0530

    Update build_steps.sh

--- azure/build_steps.sh
+++ azure/build_steps.sh
@@ -23,8 +23,7 @@ PYTHON_EXE=`which python`
 echo "##vso[task.setvariable variable=PYTHON_EXE]$PYTHON_EXE"
 echo " Define build env variables "
 /opt/_internal/cpython-3.6.11/bin/python -m pip install --upgrade pip
-/opt/_internal/cpython-$1*/bin/python -m pip install virtualenv
-export PATH=/opt/_internal/cpython-3.6.11/bin/virtualenv:$PATH
+yum install virtualenv
 echo $PATH
 BUILD_DEPENDS="numpy==1.13.3 cython==0.29.14 scipy"
 source multibuild/common_utils.sh
