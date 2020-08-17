c54cde16f50122d0aca46246381eebdce325c993
ssps12 <38716735+ssps12@users.noreply.github.com>
2020-08-17 18:41:12 +0530

    Update build_steps.sh

--- azure/build_steps.sh
+++ azure/build_steps.sh
@@ -23,8 +23,8 @@ echo "##vso[task.setvariable variable=PYTHON_EXE]$PYTHON_EXE"
 echo " Define build env variables "
 # /opt/hostedtoolcache/Python/*/x64/bin/python3 -m pip install --upgrade pip
 # yum install gcc gcc-c++ python3-devel wget make enchant-devel -y
-rm /var/lib/dpkg/lock 
-rm /var/lib/apt/lists/lock
+sudo rm /var/lib/dpkg/lock 
+sudo rm /var/lib/apt/lists/lock
 apt-get install -y python-virtualenv 
 BUILD_DEPENDS="numpy==1.13.3 cython==0.29.14 scipy"
 source multibuild/common_utils.sh
