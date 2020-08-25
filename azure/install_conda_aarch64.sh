36b81f3d60009466674a1ea38c4bc4e73a95caaa
ssps12 <38716735+ssps12@users.noreply.github.com>
2020-08-25 12:00:47 +0530

    Update install_conda_aarch64.sh

--- azure/install_conda_aarch64.sh
+++ azure/install_conda_aarch64.sh
@@ -1,11 +1,10 @@
 set -v -e
 
 # Install Miniconda
-wget -q "https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh" -O archiconda.sh
-chmod +x archiconda.sh
-sudo find / -type f -name /lib/ld-linux-aarch64.so.1
-./archiconda.sh -b -p /opt/conda
-export PATH="/opt/conda/bin:$PATH"
+wget -q "https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh"  -O miniconda.sh
+chmod +x miniconda.sh
+./miniconda.sh -b -p $HOME/miniconda3
+export PATH=$HOME/miniconda3/bin:$PATH
 conda --version
 hash -r
 conda config --set always_yes yes --set changeps1 no
