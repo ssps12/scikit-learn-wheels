90d03d98a9157a06bacb0af11b83d4725798da5c
ssps12 <38716735+ssps12@users.noreply.github.com>
2020-08-25 18:49:43 +0530

    Update install_conda_aarch64.sh

--- azure/install_conda_aarch64.sh
+++ azure/install_conda_aarch64.sh
@@ -1,11 +1,59 @@
-set -v -e
+#set -v -e
 
 # Install Miniconda
-wget -q "https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh"  -O miniconda.sh
-chmod +x miniconda.sh
-./miniconda.sh -b -p $HOME/miniconda3
-export PATH=$HOME/miniconda3/bin:$PATH
-conda --version
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
+#wget -q "https://github.com/conda-forge/miniforge/releases/download/4.8.2-1/Miniforge3-4.8.2-1-Linux-aarch64.sh"  -O miniconda.sh
+#chmod +x miniconda.sh
+#./miniconda.sh -b -p $HOME/miniconda3
+#export PATH=$HOME/miniconda3/bin:$PATH
+#conda --version
+#hash -r
+#conda config --set always_yes yes --set changeps1 no
+#conda update -q conda
+
+#!/bin/bash
+
+set -v -e
+
+if [ `uname -m` = 'aarch64' ]; then
+    rm -rf $HOME/.condarc
+fi
+
+# first configure conda to have more tolerance of network problems, these
+# numbers are not scientifically chosen, just merely larger than defaults
+conda config --write-default
+conda config --set remote_connect_timeout_secs 30.15
+conda config --set remote_max_retries 10
+conda config --set remote_read_timeout_secs 120.2
+if [[ $(uname) == Linux ]]; then
+    if [[ "$CONDA_SUBDIR" != "linux-32" && "$BITS32" != "yes" ]] ; then
+        conda config --set restore_free_channel true
+    fi
+fi
+conda info
+conda config --show
+
+CONDA_INSTALL="conda install -q -y"
+PIP_INSTALL="pip install -q"
+
+
+EXTRA_CHANNELS=""
+if [ "${USE_C3I_TEST_CHANNEL}" == "yes" ]; then
+    EXTRA_CHANNELS="${EXTRA_CHANNELS} -c c3i_test"
+fi
+
+
+# Deactivate any environment
+source deactivate
+# Display root environment (for debugging)
+conda list
+# Clean up any left-over from a previous build
+# (note workaround for https://github.com/conda/conda/issues/2679:
+#  `conda env remove` issue)
+conda remove --all -q -y -n $CONDA_ENV
+
+# If VANILLA_INSTALL is yes, then only Python, NumPy and pip are installed, this
+# is to catch tests/code paths that require an optional package and are not
+# guarding against the possibility that it does not exist in the environment.
+# Create a base env first and then add to it...
+# gitpython needed for CI testing
+$CONDA_INSTALL numpy scipy cython
